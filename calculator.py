a5e1594a3ca1b0d2e599720d334e54b58f851346
ntthanhdat <thanhdatnguyenthe@gmail.com>
2021-04-15 00:16:52 +0700

    done

--- calculator.py
+++ calculator.py
@@ -1,44 +1,39 @@
 import pandas  as pd #Data manipulation
+from sklearn import linear_model
 import numpy as np #Data manipulation
 import matplotlib.pyplot as plt # Visualization
 import os
-import csv
 
-
-
-path = 'house_Datasets.csv'
+plt.rcParams['figure.figsize'] = [8,5]
+plt.rcParams['font.size'] =14
+plt.rcParams['font.weight']= 'bold'
+#path ='dataset/'
+path = 'datafull.csv'
 dataset = pd.read_csv(path)
 #print('\nNumber of rows and columns in the data set: ',dataset.shape)
-#print('')
+
 Y = dataset[['price']]
 
-X = dataset.drop(['price', 'id', 'year'],  axis=1)
+X = dataset.drop(['price'],  axis=1)
 
-X.info()
-#print(X)
+#X.info()
+#print(Y)
 x = np.array(X.values)
 y = np.array(Y.values)
 #print(type(x))
-one = np.ones((X.shape[0], 1))
-Xbar = np.concatenate((one, X), axis = 1)
-
-# Calculating weights of the fitting line 
-A = np.dot(Xbar.T, Xbar)
-b = np.dot(Xbar.T, y)
-w = np.dot(np.linalg.pinv(A), b)
-w_0 = w[0][0]
-w_1 = w[1][0]
-w_2 = w[2][0]
-w_3 = w[3][0]
-w_4 = abs(w[4][0])
-#print('tham so ham mat mat w = ', w)
+regr = linear_model.LinearRegression()
+regr.fit(x, y)
+w=regr.coef_
+#=============
 #print to argument file
+
 if os.path.exists("argument.csv"):
   os.remove("argument.csv")
 #f = open("argument.csv", "a")
 
+
 #stringdata=str(w_0)+","+str(w_1)+","+str(w_2)+","+str(w_3)+","+str(w_4)
-dataInt=pd.DataFrame([[w_0, w_1, w_2, w_3, w_4]], columns=['w_0','w_1','w_2','w_3','w_4'])
+dataInt=pd.DataFrame([[ w[0][0],w[0][1],w[0][2],w[0][3],w[0][4],w[0][5],w[0][6],w[0][7],w[0][8],w[0][9],w[0][10],w[0][11],regr.intercept_[0]]], columns=['w_0','w_1','w_2','w_3','w_4','w_5','w_6','w_7','w_8','w_9','w_10','w_11','w_12'])
 dataInt.to_csv("argument.csv", index=False)
 #f.write(stringdata)
 #f.close()
--- test.py
+++ test.py
@@ -1,12 +1,8 @@
 import numpy as np #Data manipulation
 import pandas  as pd #Data manipulation
+
 dataset = pd.read_csv("argument.csv",  header=None, skiprows=[0])
 w = np.array(dataset.values)
 print(w)
-w_0 = w[0][0]
-w_1 = w[0][1]
-w_2 = w[0][2]
-w_3 = w[0][3]
-w_4 = w[0][4]
-y1 = w_0+ w_1*2+w_2*1+w_3*1+w_4*100
+price= w[0][0]*2+ w[0][1]*1+w[0][2]*181+w[0][3]*5650+w[0][4]*floor+ w[0][12]
 print( u'Du doan gia nha co dien tich 537 m2: %.2f (USD)' %(y1))
